91c6bf60d104963dadba45d1d0e07e532832a599
thiago <thiagoluiz_16@hotmail.com>
2020-09-27 12:53:32 -0300

    refactor: remove mongoose

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
-const mongoose = require('mongoose')
 
 const app = express()
 
-const name = process.env.DB_NAME
-const password = process.env.DB_PASSWORD
-
-mongoose.connect(`mongodb+srv://dev:${password}@developer.kuj4m.gcp.mongodb.net/${name}?retryWrites=true&w=majority`, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connection succeed")
-}).catch((err) => {
-    console.log(err)
-})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
