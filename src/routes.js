f3a72573627e3176229b370112e656c9443e57ad
thiago <thiagoluiz_16@hotmail.com>
2020-09-27 15:29:24 -0300

    refactor: add \n between texts on / page

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = express.Router()
 
 routes.get('/', async (req, res) => {
     await pullPhrases()
-    res.send("Type /phrases/random, or /phrases/seven <=> Phrases from: https://www.aluralingua.com.br/artigos/principais-frases-usadas-no-cotidiano-em-ingles")
+    res.send(`/phrases/random or /phrases/seven\n\n\nPhrases from: https://www.aluralingua.com.br/artigos/principais-frases-usadas-no-cotidiano-em-ingles`)
 })
 
 routes.get('/phrases/random', randomPhrase)
