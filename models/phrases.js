4ace26349928afa804251186a93b0672f82dc1ac
thiago <thiagoluiz_16@hotmail.com>
2020-09-27 15:53:24 -0300

    docs: remove models folder

--- models/phrases.js
+++
@@ -1,7 +0,0 @@
-  
-const mongoose = require('mongoose')
-
-module.exports = mongoose.model("phrases", new mongoose.Schema({
-    id: {type: Number, required: true, unique: true},
-    phrases: {type: String, required: true},
-}))
