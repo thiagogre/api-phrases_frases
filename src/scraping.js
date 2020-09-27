eaaeff4306f94cbcddf860375858e0eae44b27cd
thiago <thiagoluiz_16@hotmail.com>
2020-09-27 15:13:08 -0300

    feat: sep phrase and frase inside on object

--- src/scraping.js
+++ src/scraping.js
@@ -10,6 +10,28 @@ const pullPhrases = () => {
             const phrase = $(element).text()
             phrases[index] = {"id":index, phrase}
         })
+
+        let store_items = []
+        let refactor_store_items = []
+        phrases.map(phrase => {
+            store_items.push(phrase['phrase'])
+        })
+        for (item of store_items) {
+            if (item.includes('-')) {
+                holder = item.split('-')
+            } else {
+                holder = item.split('–')
+            }
+            refactor_store_items.push(holder)
+        }
+
+        let n = 0
+        for (i of phrases) {
+            phrases[n]['phrase'] = refactor_store_items[n][0]
+            phrases[n]['frase'] = refactor_store_items[n][1]
+            n += 1
+        }
+        
         fs.writeFile("data.json", JSON.stringify(phrases, null, 2), (err) => {
             if(err) throw err
         })
