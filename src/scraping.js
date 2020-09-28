aaa36786573e1f17d87e962696e2f82659346ec2
thiago <thiagoluiz_16@hotmail.com>
2020-09-27 21:30:00 -0300

    refactor: functional programming apply

--- src/scraping.js
+++ src/scraping.js
@@ -2,39 +2,30 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
-const pullPhrases = () => {
-    axios.get('https://www.aluralingua.com.br/artigos/principais-frases-usadas-no-cotidiano-em-ingles').then((res) => {
-        let phrases = []
-        const $ = cheerio.load(res.data)
-        $('.cosmos-container > ul > li > p').each((index, element) => {
-            const phrase = $(element).text()
-            phrases[index] = {"id":index, phrase}
-        })
+const pullPhrases = async () => {
+    const res = await axios.get('https://www.aluralingua.com.br/artigos/principais-frases-usadas-no-cotidiano-em-ingles')
+    let phrases = []
+    const $ = cheerio.load(res.data)
+    $('.cosmos-container > ul > li > p').each((index, element) => {
+        const phrase = $(element).text()
+        phrases[index] = {"id":index, phrase}
+    })
 
-        let store_items = []
-        let refactor_store_items = []
-        phrases.map(phrase => {
-            store_items.push(phrase['phrase'])
-        })
-        for (item of store_items) {
-            if (item.includes('-')) {
-                holder = item.split('-')
-            } else {
-                holder = item.split('–')
-            }
-            refactor_store_items.push(holder)
-        }
+    const store_items = phrases.map(phrase => (phrase['phrase']))
+    const refactor_store_items = store_items.map(item => item.includes('-') ? item.split('-'):item.split('–'))
+    phrases.forEach((phrase, index) => {
+        phrase['phrase'] = refactor_store_items[index][0].substring(0, refactor_store_items[index][0].length - 1)
+        phrase['frase'] = refactor_store_items[index][1].substring(1)
+    })
+    // let n = 0
+    // for (i of phrases) {
+        
+        
+    //     n += 1
+    // }
 
-        let n = 0
-        for (i of phrases) {
-            phrases[n]['phrase'] = refactor_store_items[n][0].substring(0, refactor_store_items[n][0].length - 1)
-            phrases[n]['frase'] = refactor_store_items[n][1].substring(1)
-            n += 1
-        }
-
-        fs.writeFile("data.json", JSON.stringify(phrases, null, 2), (err) => {
-            if(err) throw err
-        })
+    fs.writeFile("data.json", JSON.stringify(phrases, null, 2), (err) => {
+        if(err) throw err
     })
 }
 
