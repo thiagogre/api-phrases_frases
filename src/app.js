750e8d03ae687548956971837f16cdb8e7ff1710
thiago <thiagoluiz_16@hotmail.com>
2020-09-28 22:39:47 -0300

    chore: connect to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(`mongodb+srv://dev:u6CeJtS6YL9czFFP@developer.kuj4m.gcp.mongodb.net/database?retryWrites=true&w=majority`, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection Done")
+}).catch((err) => {
+    console.log(err)
+})
 
 app.use(express.json())
 app.use(routes)
