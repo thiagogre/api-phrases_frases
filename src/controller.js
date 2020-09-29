543a0ec74173cad73a9c7f4e05ccc2820794c52c
thiago <thiagoluiz_16@hotmail.com>
2020-09-28 23:52:58 -0300

    chore: remove _id and __v from res.json

--- src/controller.js
+++ src/controller.js
@@ -2,13 +2,13 @@ const Phrase = require('./models/Phrase')
 const randomN = require('./utils/random_number')
 
 const random_phrase = async (req, res) => {
-  const phrase = await Phrase.find({})
+  const phrase = await Phrase.find({}, {__v:0,_id:0})
   const random_phrase = phrase[Math.floor(Math.random() * phrase.length)]
   res.json(random_phrase)
 }
 
 const random_phrase_seven = async (req, res) => {
-    const phrase = await Phrase.find({})
+    const phrase = await Phrase.find({}, {__v:0,_id:0})
     const random_phrase_seven = randomN(phrase, 7)
     res.json(random_phrase_seven)
 }
