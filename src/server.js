3ed682d2e9b617809c70c26f98bb257addbe5055
thiago <thiagoluiz_16@hotmail.com>
2020-10-20 12:22:11 -0300

    style: change file name

--- src/server.js
+++ src/server.js
@@ -3,21 +3,19 @@ const routes = require('./routes')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-const app = express()
+const server = express()
 
 dotenv.config()
 const name = process.env.DB_NAME
 const pass = process.env.DB_PASSWORD
 
-mongoose.connect(`mongodb+srv://dev:${pass}@developer.kuj4m.gcp.mongodb.net/${name}?retryWrites=true&w=majority`, { 
+mongoose.connect(`mongodb+srv://dev:${pass}@developer.kuj4m.gcp.mongodb.net/${name}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  console.log("Connection Done")
+  console.log('Connection Done')
 }).catch((err) => {
   console.log(err)
 })
 
-app.use(express.json())
-app.use(routes)
-app.listen(3000, () => console.log(`listening on 3000`))
+server.use(express.json()).use(routes).listen(3000, () => console.log('listening on 3000'))
