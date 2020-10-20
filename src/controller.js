e8ce905d38a19e5dfdd00b6b874942acce32221c
thiago <thiagoluiz_16@hotmail.com>
2020-10-20 12:20:35 -0300

    style: change variables name

--- src/controller.js
+++ src/controller.js
@@ -2,18 +2,18 @@ const Phrase = require('./models/Phrase')
 const randomN = require('./utils/random_number')
 
 const randomPhrase = async (req, res) => {
-  const phrase = await Phrase.find({}, {__v:0,_id:0})
-  const random_phrase = phrase[Math.floor(Math.random() * phrase.length)]
-  res.json(random_phrase)
+  const phrase = await Phrase.find({}, { __v: 0, _id: 0, id: 0 })
+  const randomPhrase = phrase[Math.floor(Math.random() * phrase.length)]
+  res.json(randomPhrase)
 }
 
 const randomPhraseSeven = async (req, res) => {
-    const phrase = await Phrase.find({}, {__v:0,_id:0})
-  const random_phrase_seven = randomN(phrase, 7)
-  res.json(random_phrase_seven)
+  const phrase = await Phrase.find({}, { __v: 0, _id: 0, id: 0 })
+  const randomPhraseSeven = randomN(phrase, 7)
+  res.json(randomPhraseSeven)
 }
 
-module.exports = { 
+module.exports = {
   randomPhrase,
   randomPhraseSeven
 }
